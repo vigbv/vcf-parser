7991ed2d275e195a89beb82119cef01bd87c5434
AM\vbrahmad <vbrahmad@amgen.com>
2022-10-13 19:31:30 -0700

    adding comments

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import re
 import sys
 from datetime import datetime
 
-start_time = datetime.now()
+start_time = datetime.now() # track execution time
 class Vcf:
   def __init__(self, file_name):
     self.file_name = file_name
@@ -14,22 +14,22 @@ class Vcf:
     with open(self.file_name) as file:
         for line in file: # loop through each line of input vcf file
             line = line.rstrip()
-            if line.startswith("##"):
+            if line.startswith("##"): # process metadata
                 metadata.append(line)
-            elif line.startswith("#CHROM"):
+            elif line.startswith("#CHROM"): # process header columns
                 header_line = re.split(r"\t+", line)
                 num_columns = len(header_line)
             else:
-                for sample_num in range(9, num_columns): 
+                for sample_num in range(9, num_columns): # loop through samples and write to output 
                     sample_name = header_line[sample_num]
-                    if sample_name not in file_names:
+                    if sample_name not in file_names: # open file handle only for unprocessed samples
                         file_names[sample_name] =  open(sample_name + '.vcf', 'w')
                         file_names[sample_name].write("\n".join(metadata) + "\n" + "\t".join(header_line[0:9]) + "\t" + sample_name + "\n")
                     
                     split_line = re.split(r"\t+", line)
                     file_names[sample_name].write("\t".join(split_line[0:9]) + "\t" + split_line[sample_num] + "\n")
                     
-    for key in file_names:            
+    for key in file_names: # close file handles
         file_names[key].close()
 
 vcf_file = sys.argv[1]
