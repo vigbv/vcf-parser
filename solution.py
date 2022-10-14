fc12b0e67e0dc43e9c3553a895d53439d00b1288
AM\vbrahmad <vbrahmad@amgen.com>
2022-10-13 19:09:49 -0700

    init

--- solution.py
+++ solution.py
@@ -1,26 +1,37 @@
 import re
+from datetime import datetime
 
-metadata = [] # list to store metadata
-file_names = {} # capture file names to manage file handlers
+start_time = datetime.now()
+class Vcf:
+  def __init__(self, file_name):
+    self.file_name = file_name
 
-with open("test_1.vcf") as file:
+  def vcfParse(self):
+    metadata = [] # list to store metadata
+    file_names = {} # capture file names to manage file handlers
     
-    for line in file: # loop through each line of input vcf file
-        line = line.rstrip()
-        if line.startswith("##"):
-            metadata.append(line)
-        elif line.startswith("#CHROM"):
-            header_line = re.split(r"\t+", line)
-            num_columns = len(header_line)
-            
-        else:
-            for i in range(9, num_columns): 
-                if header_line[i] not in file_names:
-                    file_names[header_line[i]] =  open(header_line[i] + '.vcf', 'w')
-                    file_names[header_line[i]].write("\n".join(metadata) + "\n" + "\t".join(header_line[0:8]) + "\t" + header_line[i] + "\n")
-                  
-                split_line = re.split(r"\t+", line)
-                file_names[header_line[i]].write("\t".join(split_line[0:8]) + "\t" + split_line[i] + "\n")
-                
+    with open(self.file_name) as file:
+        for line in file: # loop through each line of input vcf file
+            line = line.rstrip()
+            if line.startswith("##"):
+                metadata.append(line)
+            elif line.startswith("#CHROM"):
+                header_line = re.split(r"\t+", line)
+                num_columns = len(header_line)
+            else:
+                for sample_num in range(9, num_columns): 
+                    sample_name = header_line[sample_num]
+                    if sample_name not in file_names:
+                        file_names[sample_name] =  open(sample_name + '.vcf', 'w')
+                        file_names[sample_name].write("\n".join(metadata) + "\n" + "\t".join(header_line[0:8]) + "\t" + sample_name + "\n")
+                    
+                    split_line = re.split(r"\t+", line)
+                    file_names[sample_name].write("\t".join(split_line[0:8]) + "\t" + split_line[sample_num] + "\n")
+                    
     for key in file_names:            
         file_names[key].close()
+
+vcf = Vcf("test.vcf")
+vcf.vcfParse()
+
+print(datetime.now() - start_time)
